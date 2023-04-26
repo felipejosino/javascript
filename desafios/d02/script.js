9dd3fe7ce8f5e819f125b5ee80d347fd629a9587
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-04-26 15:55:23 -0300

    Atualizado validação de dados d02

--- desafios/d02/script.js
+++ desafios/d02/script.js
@@ -5,7 +5,24 @@ function dados(){
     let nome = window.prompt(`Qual é o seu nome?`)
     let idade  = window.prompt(`Quantos anos voce tem ? [Apenas numeros]`)
 
-    // window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade`)
+    let reLetra = new RegExp('[^a-zA-Z \u00C0-\u00FF]','gi')
+    let reNumero = new RegExp('[^0-9]')
+
+    if(nome == "" || nome == " " || reLetra.test(nome)){
+        print.innerHTML = '[ERRO] Preencha o campo nome corretamente!'
+        return
+    }
+
+    if(reNumero.test(idade) || !isFinite(idade)){
+        print.innerHTML = '[ERRO] Preencha o campo idade corretamente!'
+        return 
+    }
+
+    if(idade == ""){
+         idade = 0
+    }
+
+
     print.innerHTML = `Acabei de conhecer ${nome}, que tem ${idade} anos de idade.`
 }
 
