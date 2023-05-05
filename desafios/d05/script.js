2bef629d38639cd1d1c98d162be3209f968214c4
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-05-05 14:17:48 -0300

    atualizado validações com regex

--- desafios/d05/script.js
+++ desafios/d05/script.js
@@ -4,8 +4,6 @@ var print = window.document.querySelector('div#print')
 
 function converterMedida(){
 
-// ---------------Teste------------------
-
 let input = window.prompt(`Digite uma distância em metros (m)     ex: 875.29`)
 
 if( input == null){
@@ -13,14 +11,9 @@ if( input == null){
     return
 }
 
-if( input == ""){
-    print.innerHTML = `Informe um valor para conversão!`
-    return
-}
-
 let metros = input.replace(',', '.')
 
-if(metros == 0){
+if(input == "" || metros == 0){
     print.innerHTML = `Informe um valor para conversão!`
     return
 }
@@ -30,7 +23,6 @@ let regexNum = new RegExp('^0-9')
 function verificarNum(num){
     return regexNum.test(parseFloat(num)) || !isFinite(num)
 }
-
 
 if( verificarNum(metros) ){
         print.innerHTML = `[ERRO] Preencha valor do campo corretamente!`
@@ -60,33 +52,6 @@ print.innerHTML =
 
     `
     console.log(verificarNum(metros))
-
-
-
-
-// ---------------Funcionando------------------
-// let metros = window.prompt(`Digite uma distância em metros (m) ex: 875.29`)
-        
-// let km = parseFloat(metros /1000).toFixed(3).replace('.', ',')
-// let hm = parseFloat(metros /100).toFixed(3).replace('.', ',')
-// let dam = parseFloat(metros /10).toFixed(3).replace('.', ',')
-
-// let dm = parseFloat(metros *10).toLocaleString()
-// let cm = parseFloat(metros *100).toLocaleString()
-// let mm = parseFloat(metros *1000).toLocaleString()
-
-// print.innerHTML = 
-//     `<h2>A distância ${metros.replace('.', ',')} <small>metros</small>, corresponde a...</h2>
-//     <br>
-//     <p>${km} quilômetros (Km)</p> <br>
-//     <p>${hm} hectômetros (hm)</p> <br>
-//     <p>${dam} decâmetros (dam)</p> <br>
-//     <p>${dm} decímetros (dm)</p> <br>
-//     <p>${cm} centímetros (cm)</p> <br>
-//     <p>${mm} milímetros (mm)</p>
-
-//     `
-
 }
 
 btn.addEventListener('click', function() {
