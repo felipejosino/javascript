87d2ae384eb8f9cada5fb53125b47f2e080feced
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-05-09 20:49:42 -0300

    Atualizado validações com regex

--- desafios/d07/script.js
+++ desafios/d07/script.js
@@ -3,21 +3,65 @@ var print = window.document.querySelector(`div#print`)
 
 function converterReal(){
 
-    window.alert(`Desafio em manutenção, voltaremos em breve...`)
-
-    // let valorDolar = parseFloat(window.prompt(`Antes de mais nada. Quanto está a cotação do dólar agora ?`))
-
-    // let valorReais = parseFloat(window.prompt(`Quantos R$ você tem na carteira ?`))
-
-    // //Cotação 09/05/2023  5,00 Real = 1 Dólar
+    let inputDolar = window.prompt(`Antes de mais nada. Quanto está a cotação do dólar agora ?`)
     
-    // let valorConvertido = (valorReais / valorDolar)
+    let regexNum = new RegExp('^0-9')
 
-    // print.innerHTML = 
-    // `   <p>Cotação do dólar : ${valorDolar.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</p>
-    //     <h2>Seu valor ${valorReais.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} convertido em dólar é ${valorConvertido.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</h2>
-    //     `
-    //     console.log(`${valorReais} , ${valorDolar}, ${valorConvertido}`)
+    if( inputDolar == null ){
+        print.innerHTML = `<p>Informe o valor da cotação do Dólar</p>`
+        console.log( inputDolar )
+        return
+    }
+
+    let valorDoDolar = parseFloat( inputDolar.replace(',' , '.') )
+
+    function verificarNum(num){
+        return regexNum.test( parseFloat(num) ) || !isFinite(num)
+    }
+    
+    if( verificarNum(valorDoDolar) ){
+        print.innerHTML = `<p>[ERRO] Preencha valor da cotação corretamente!</p>`
+        console.log( verificarNum(valorDoDolar) )
+        return
+    } 
+
+    function statusDesafio(status){
+        return status == null
+    }
+    
+    if( statusDesafio(valorDoDolar) ){
+        print.innerHTML = `<p>Voce cancelou desafio!</p>`
+        console.log( statusDesafio(valorDoDolar) )
+        return
+    }
+
+    let inputReal = window.prompt(`Quantos R$ você tem na carteira ?`)
+
+    if( statusDesafio(inputReal) || inputReal == "" ){
+        print.innerHTML = `<p>Insira o valor que voce possui para converter!</p>`
+        console.log( statusDesafio(inputReal) )
+        return
+    }
+
+    let valorDoReal = parseFloat( inputReal.replace(',' , '.') )
+
+    if( verificarNum(valorDoReal) ){
+        print.innerHTML = `<p>[ERRO] Preencha valor que possui para conversão corretamente!</p>`
+        console.log( verificarNum(valorDoReal) )
+        return
+    } 
+
+    //Cotação 09/05/2023  5,00 Real = 1 Dólar
+    
+    let valorConvertido = (valorDoReal / valorDoDolar)
+
+    print.innerHTML = 
+    `   
+    <p><small>Cotação do dólar :</small> ${valorDoDolar.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</p>
+    <br>
+    <h2><small>Seu valor</small> ${valorDoReal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <small>convertido em dólar é</small> ${valorConvertido.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</h2>
+        `
+    console.log(`${valorDoReal} , ${valorDoDolar}, ${valorConvertido}`)
 }
 
 btn.addEventListener('click', function(){
