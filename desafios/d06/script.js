025787bf14d0619c9b7214d335cbf8b1e326ddfd
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-05-09 11:51:13 -0300

    atualizado validações com regex

--- desafios/d06/script.js
+++ desafios/d06/script.js
@@ -3,19 +3,54 @@ var print = window.document.querySelector('div#print')
 
 
 function converterTemperatura(){
-    window.alert(`Desafio em manuteção...`)
+    // window.alert(`Desafio em manuteção...`)
 
-// let c = parseFloat(window.prompt(`Digite uma temperatura em °C (Celsius).  Ex: 35.7 `))
+let input = window.prompt(`Digite uma temperatura em °C (Celsius).  Ex: 35.7 `)
 
-// let k = (c + 273.15).toFixed(2)
+if( input == null){
+    print.innerHTML = `<p>Voce cancelou desafio!</p>`
+    console.log(input)
+    return
+}
 
-// let f = (c * 1.8 + 32).toFixed(2)
+if(input == ""){
+    print.innerHTML = `<p>Informe um valor para conversão!</p>`
+    console.log(input)
+    return
+}
 
-// print.innerHTML = 
-// `   <h2>A temperatura de ${c}°C, corresponde a...</h2>
-//     <p>${k}°K (Kelvin)</p>
-//     <p>${f}°F (Fahrenheit)</p>
-// `
+let celsius = parseFloat(input.replace(',', '.'))
+
+let regexNum = new RegExp('^0-9')
+
+function verificarNum(num){
+    return regexNum.test(parseFloat(num)) || !isFinite(num)
+}
+
+if( verificarNum(celsius) ){
+    print.innerHTML = `<p>[ERRO] Preencha valor do campo corretamente!</p>`
+    console.log(verificarNum(celsius))
+    return
+} 
+
+let kelvin = (celsius + 273.15).toLocaleString()
+
+let fahrenheit = (celsius * 1.8 + 32).toLocaleString()
+
+print.innerHTML = 
+`   <h2>A temperatura de ${celsius}°C, corresponde a...</h2><br>
+    <p>${kelvin}°K (Kelvin)</p>
+    <p>${fahrenheit}°F (Fahrenheit)</p>
+    
+    <br><br>
+    
+    <h3>Fórmula:</h3>
+    <br>
+    <p>Kelvin: <small><strong>${celsius}°C + 273.15</strong> = ${kelvin}°K</small></p>
+    <br>
+    <p>Fahrenheit: <small><strong>(${celsius}°C x 9/5) + 32</strong> = ${fahrenheit}°F</small></p>
+`
+    console.log(celsius)
 }
 
 btn.addEventListener('click', function() {
