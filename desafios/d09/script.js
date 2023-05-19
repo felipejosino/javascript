282f0540dce1ca38e1dee37008874002d5bed232
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-05-19 10:45:41 -0300

    Atualizado validações com regex

--- desafios/d09/script.js
+++ desafios/d09/script.js
@@ -3,24 +3,99 @@ var print = window.document.querySelector('div#print')
 
 function reajusteSalarial() {
 
-    window.alert(`Desafio em manutenção, voltaremos em breve...`)
+    // window.alert(`Desafio em manutenção, voltaremos em breve...`)
 
-    // let nomeFun = window.prompt(`Qual é o nome do Funcionário?  EX:. Pedro Paulo`)
-    // let salario = parseFloat(window.prompt(`Qual é o salário de ${nomeFun} ?  E:x. 2850.25`))
-    // let reajuste = parseFloat(window.prompt(`O salário de ${nomeFun} vai ser reajustado em qual porcentagem?  EX:. 18`))
+    let inputNomeFucnionario = window.prompt(`Qual é o nome do Funcionário?  EX:. Pedro Paulo`)
 
-    // let valorReajustado = (reajuste / 100) * salario
+    if( inputNomeFucnionario == null ){
+        print.innerHTML = `<p>Cancelou desafio!</p>`
+        return
+    }
 
-    // let salarioFinal = salario + valorReajustado
+    let nomeFuncionario = inputNomeFucnionario.split(" ").filter( value => value != "").join(" ")
 
-    // print.innerHTML = 
-    // `
-    //     <h2>${nomeFun} recebeu um aumento salarial !</h2>
+    let regexLetra = new RegExp('[^a-zA-Z \u00c0-\u00FF]', 'gi')
+    let regexNumero = new RegExp('^0-9')
 
-    //     <p>O salário atual era ${salario.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.</p>
-    //     <p>Com um aumento de ${reajuste}%, o salário vai aumentar ${valorReajustado.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} no próximo mês.</p>
-    //     <p>E a partir daí, ${nomeFun} vai passar a ganhar ${salarioFinal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
-    // `
+    function verificarNome(nomeProduto){
+        return  nomeProduto == "" || regexLetra.test(nomeProduto)
+    }
+
+    if (verificarNome(nomeFuncionario)){
+        print.innerHTML = `<p>[ERRO] Preencha o campo nome corretamente!</p>`
+        console.log(verificarNome(nomeFuncionario))
+        return
+    }
+
+    let inputSalario = window.prompt(`Qual é o salário de ${nomeFuncionario} ?  E:x. 2850.25`)
+
+    if( inputSalario == null ){
+        print.innerHTML = `<p>Informe o valor do salario</p>`
+        console.log( inputSalario )
+        return
+    }
+
+    let salario =  parseFloat( inputSalario.replace(',' , '.') )
+
+    function verificarNum(num){
+        return regexNumero.test( parseFloat(num) ) || !isFinite(num)
+    }
+    
+    if( verificarNum(salario) ){
+        print.innerHTML = `<p>[ERRO] Preencha valor do salario corretamente!</p>`
+        console.log( verificarNum(salario) )
+        return
+    } 
+
+    function statusDesafio(status){
+        return status == null
+    }
+    
+    if( statusDesafio(salario) ){
+        print.innerHTML = `<p>Voce cancelou desafio!</p>`
+        console.log( statusDesafio(salario) )
+        return
+    }
+
+    let inputReajuste = window.prompt(`O salário de ${nomeFuncionario} vai ser reajustado em qual porcentagem?  EX:. 18`)
+
+    if( inputReajuste == null ){
+        print.innerHTML = `<p>Informe o valor do reajuste</p>`
+        console.log( inputReajuste )
+        return
+    }
+
+    let reajuste = parseFloat( inputReajuste.replace(',' , '.') )
+
+    if( verificarNum(reajuste) ){
+        print.innerHTML = `<p>[ERRO] Preencha valor do reajuste corretamente!</p>`
+        console.log( verificarNum(reajuste) )
+        return
+    } 
+
+    if( statusDesafio(reajuste) ){
+        print.innerHTML = `<p>Voce cancelou desafio!</p>`
+        console.log( statusDesafio(reajuste) )
+        return
+    }
+
+    let valorReajustado = (reajuste / 100) * salario
+
+    let salarioFinal = salario + valorReajustado
+
+    print.innerHTML = 
+    `
+        <h2>${nomeFuncionario} recebeu um aumento salarial !</h2>
+        <br>
+        <p>O salário atual era ${salario.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.</p>
+        <br>
+        <p>Com um aumento de ${reajuste}%, o salário vai aumentar ${valorReajustado.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} no próximo mês.</p>
+        <br>
+        <p>E a partir daí, ${nomeFuncionario} vai passar a ganhar ${salarioFinal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
+    `
+    console.log(nomeFuncionario)
+    console.log(salario)
+    console.log(reajuste)
 
 }
 
