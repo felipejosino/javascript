d2f3e0e94b6e38381b1aa2a3a9a8214714e23ddb
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-05-19 17:53:20 -0300

    removido seletor css

--- desafios/d11/script.js
+++ desafios/d11/script.js
@@ -2,28 +2,48 @@ var btn = window.document.querySelector('input#btn')
 var print = window.document.querySelector('div#print')
 
 function verificarAnoBissexto() {
+    // window.alert(`Desafio em manutenção, voltaremos em breve...`)
 
-    window.alert(`Desafio em manutenção, voltaremos em breve...`)
+    let inputAno = window.prompt(`Qual é o ano que você quer verificar ?`)
 
-    // var btn = window.document.querySelector(`input#btn`)
-    // var print = window.document.querySelector(`div#print`)
+    let regexNum = new RegExp('[^0-9]')
+
+    function statusDesafio(num){
+        return num == null
+    }
     
-    // btn.addEventListener('click', function(){
-    //     let ano = window.prompt(`Qual é o ano que você quer verificar ?`)
-    
-    //     if(ano.length > 4){
-    //         print.innerHTML = `<mark id="vermelho">[ERRO] Valor inválido</mark>`
-    //         return
-    //     }
-    
-    //     if(ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0){
-    //         print.innerHTML = `O ano ${ano} <mark id="verde">É BISSEXTO</mark> \u{2705}`
-    //         return
-    //     }
-    //     print.innerHTML = `O ano ${ano} <mark id="vermelho">NÃO É BISSEXTO</mark> \u{274C}`
-    
-      
-    // })
+    function verificarNum(num){
+        return regexNum.test( parseInt(num) ) || !isFinite(num)
+    }
+
+    if( statusDesafio(inputAno) ){
+        print.innerHTML = `<p>Cancelou desafio!</p>`
+        console.log( statusDesafio(inputAno) )
+        console.log( inputAno )
+        return
+    }
+
+    if(inputAno.length > 4){
+        print.innerHTML = `<mark id="vermelho">[ERRO] Valor inválido</mark>`
+        console.log( verificarNum(inputAno) )
+        console.log( inputAno )
+        return
+    }
+
+    ano = parseInt ( inputAno )
+
+    if( verificarNum( ano ) ){
+        print.innerHTML = `<p><mark id="vermelho">[ERRO] Preencha valor do Ano corretamente!</mark></p>`
+        console.log( verificarNum(ano) )
+        console.log( ano )
+        return
+    } 
+
+    if(ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0){
+        print.innerHTML = `O ano ${ano} <mark id="verde">É BISSEXTO</mark> \u{2705}`
+        return
+    }
+    print.innerHTML = `O ano ${ano} <mark id="vermelho">NÃO É BISSEXTO</mark> \u{274C}`
 }
 
 btn.addEventListener('click', function(){
