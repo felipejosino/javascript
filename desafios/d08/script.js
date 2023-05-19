c7cb993ab0b919b142772a2ca73e0bc0e23874a1
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-05-19 10:16:21 -0300

    Atualizado validações com regex

--- desafios/d08/script.js
+++ desafios/d08/script.js
@@ -74,27 +74,6 @@ function calcularDesconto() {
     console.log(nomeProduto)
     console.log(valorDesconto)
     console.log(valorFinal)
-
-
-
-
-    // ---------------- funcionando ---------------------
-
-    // let nomeProd = window.prompt(`Qual é o produto que você está comprando?`)
-    // let valorProd = parseFloat(window.prompt(`Qual é o preço de ${nomeProd}?  Ex:. 795.98`))
-    
-    // let valorDesconto = (10 / 100) * valorProd
-    // let valorFinal = valorProd - valorDesconto
-    
-    // print.innerHTML = 
-    // `
-    //     <h2>Calculando desconto de 10% para ${nomeProd}</h2>
-    //     <p>O Preço original era ${valorProd.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.</p>
-    //     <p>Você acaba de ganhar ${valorDesconto.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})} de desconto (-10%).</p>
-    //     <p>No fim, você vai pagar ${valorFinal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} no produto ${nomeProd}</p>
-
-    // `
-
 }
 
 btn.addEventListener('click', function(){
