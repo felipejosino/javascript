b21a13d48536e6fbeb78f9ac234d0af783873361
Felipe Josino <127350307+felipejosino@users.noreply.github.com>
2023-05-22 20:33:40 -0300

    atualizado validações em regex

--- desafios/d12/script.js
+++ desafios/d12/script.js
@@ -3,44 +3,86 @@ var print = window.document.querySelector(`div#print`)
 
 function verificarPreco(){
 
-    window.alert(`Desafio em manutenção, voltaremos em breve...`)
+    // window.alert(`Desafio em manutenção, voltaremos em breve...`)
 
-    // let precoAntigo = parseFloat(window.prompt(`Qual era o preço do produto?`))
-    // let precoAtual = parseFloat(window.prompt(`Qual é o preço atual do produto ?`))
 
-    // if(precoAntigo < precoAtual){
+    let inputPrecoAntigo = window.prompt(`Qual era o preço do produto?`)
 
-    //     let diferenca = precoAtual - precoAntigo
-    //     let variacaoSubiu = (precoAtual - precoAntigo) / precoAntigo * 100
-        
-    //     print.innerHTML = ` 
-        
-    //     <p>O produto custava ${precoAntigo.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} e agora custa ${precoAtual.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.</p>
+    let regexNum = new RegExp('^0-9')
 
-    //     <p>Hoje o produto está mais caro.</p>
-
-    //     <p>O preço subiu ${diferenca.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} em relação ao preço anterior.</p>
-
-    //     <p>Uma variação de ${variacaoSubiu.toFixed(2)}% para cima.</p>
-        
-    //     `
-    //     return
-    // }
+    function statusDesafio(status){
+        return status == null
+    }
     
-    // let diferenca =  precoAntigo - precoAtual
-    // let variacaoCaiu = ( precoAntigo - precoAtual) / precoAntigo * 100
+    function verificarNum(num){
+        return regexNum.test( parseFloat(num) ) || !isFinite(num)
+    }
 
-    // print.innerHTML = 
-    //     `
-    //     <p>O produto custava ${precoAntigo.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} e agora custa ${precoAtual.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.</p>
+    if( statusDesafio(inputPrecoAntigo) ){
+        print.innerHTML = `<p>Cancelou desafio!</p>`
+        console.log( statusDesafio(inputPrecoAntigo) )
+        console.log( inputPrecoAntigo )
+        return
+    }
 
-    //     <p>Hoje o produto está mais barato.</p>
+    precoAntigo = parseFloat ( inputPrecoAntigo )
 
-    //     <p>O preço caiu ${diferenca.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} em relação ao preço anterior.</p>
+    if( verificarNum( precoAntigo ) ){
+        print.innerHTML = `<p><mark id="vermelho">[ERRO] Preencha valor do preço anterior do produto corretamente!</mark></p>`
+        console.log( verificarNum(inputPrecoAntigo) )
+        console.log( verificarNum(precoAntigo) )
+        console.log( precoAntigo )
+        return
+    } 
 
-    //     <p>Uma variação de ${variacaoCaiu.toFixed(2)}% pra baixo.</p>
-    //     `
-    //     return
+    let inputPrecoAtual = window.prompt(`Qual é o preço atual do produto ?`)
+
+    if( statusDesafio(inputPrecoAtual) ){
+        print.innerHTML = `<p>Cancelou desafio!</p>`
+        console.log( statusDesafio(inputPrecoAtual) )
+        console.log( inputPrecoAtual )
+        return
+    }
+
+    precoAtual = parseFloat ( inputPrecoAtual )
+
+    if( verificarNum( precoAtual ) ){
+        print.innerHTML = `<p><mark id="vermelho">[ERRO] Preencha valor do preço atual do produto corretamente!</mark></p>`
+        console.log( verificarNum(inputPrecoAntigo) )
+        console.log( verificarNum(precoAntigo) )
+        console.log( precoAntigo )
+        return
+    } 
+
+        let diferenca = precoAtual - precoAntigo
+        let variacaoDePreoco = (precoAtual - precoAntigo) / precoAntigo * 100
+        
+        print.innerHTML = ` 
+         <p>O produto custava ${precoAntigo.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} e agora custa ${precoAtual.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.</p>
+        <br>
+        `
+
+        if(precoAntigo < precoAtual){
+
+            print.innerHTML +=`
+            <p>Hoje o produto está mais caro.</p>
+            <br>
+            <p>O preço subiu ${diferenca.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} em relação ao preço anterior.</p>
+            <br>
+            <p>Uma variação de ${variacaoDePreoco.toFixed(2)}% para cima.</p>
+            `
+            return
+
+        }
+
+        print.innerHTML += `
+        <p>Hoje o produto está mais barato.</p>
+        <br>
+        <p>O preço caiu ${diferenca.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} em relação ao preço anterior.</p>
+        <br>
+        <p>Uma variação de ${variacaoDePreoco.toFixed(2)}% pra baixo.</p>
+        `
+
 }
 
 btn.addEventListener('click', function(){
